bd15d020058d38f9faa18c09ef3e7f210ab94637
Priscilla Paxton <ppaxton22@turing.edu>
2023-03-03 22:36:47 -0700

    Incorporate more DOM features of data model

--- game.js
+++ game.js
@@ -4,30 +4,23 @@ class Game {
     this.playerTwoTurn = false
     this.playerOne = new Player(1, "x")
     this.playerTwo = new Player(2, "o")
-    this.squareOne 
-    this.squareTwo 
-    this.squareThree 
-    this.squareFour 
-    this.squareFive 
-    this.squareSix 
-    this.squareSeven 
-    this.squareEight 
-    this.squareNine 
-    // this.gameBoard = gameObj
-    // this.playerOne = []
-    // this.playerTwo = []
+    this.squareOne = undefined
+    this.squareTwo = undefined
+    this.squareThree = undefined
+    this.squareFour = undefined
+    this.squareFive = undefined
+    this.squareSix = undefined
+    this.squareSeven = undefined
+    this.squareEight = undefined
+    this.squareNine = undefined
   }
-  switchTurn() {
-    if (this.playerOneTurn) {
-      this.playerTwoTurn = true 
-      this.playerOneTurn = false
-      console.log('player 2 turn')
-    } else if (this.playerTwoTurn) {
-      this.playerOneTurn = true
-      this.playerTwoTurn = false
-      console.log('player 1 turn')
-      //add unctionality to change dom display
+  updateBoard(event) {
+    if (this[event.target.id] === undefined) {
+      this[event.target.id] = currentPlayer
+      console.log(`${event.target.id} was clicked`)
+      this.checkWinStatus(currentPlayer)
     }
+    this.switchTurn()
   }
   resetBoard() {
     this.squareOne = undefined
@@ -39,15 +32,11 @@ class Game {
     this.squareSeven = undefined
     this.squareEight = undefined
     this.squareNine = undefined
-    //how do I make my conditionals check for a specific string?
-    //could I make them check to see if they all have the same value?
-    //would it be easier to check if each player's arrays contained numbers?
   }
  checkWinStatus(currentPlayer) {
   if (this.squareOne === currentPlayer && this.squareTwo === currentPlayer && this.squareThree === currentPlayer) {
     this[currentPlayer].wins += 1
-    console.log(this[currentPlayer])
-    console.log(this[currentPlayer].wins)
+    console.log(`${currentPlayer} won`)
     this.resetBoard()
   } else if (this.squareFour === currentPlayer && this.squareFive === currentPlayer && this.squareSix === currentPlayer) {
     this[currentPlayer].wins += 1
@@ -70,6 +59,17 @@ class Game {
   } else if (this.squareThree === currentPlayer && this.squareFive === currentPlayer && this.squareSeven === currentPlayer) {
     this[currentPlayer].wins += 1
     this.resetBoard()
+  }
+}
+switchTurn() {
+  if (this.playerOneTurn) {
+    this.playerTwoTurn = true 
+    this.playerOneTurn = false
+    console.log('player 2 turn')
+  } else if (this.playerTwoTurn) {
+    this.playerOneTurn = true
+    this.playerTwoTurn = false
+    console.log('player 1 turn')
   }
 }
   gameScoreCheck(playerOne, playerTwo) {
